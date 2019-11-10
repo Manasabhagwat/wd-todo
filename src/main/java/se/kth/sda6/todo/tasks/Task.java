5ffaf5caa14b3704231af1ee98ab4dcccc3102cb
Ayham Alkazaz <ayham.alkazaz@voiapp.io>
2019-11-10 18:18:28 +0100

    6. Repositories and ORMs

--- src/main/java/se/kth/sda6/todo/tasks/Task.java
+++ src/main/java/se/kth/sda6/todo/tasks/Task.java
@@ -1,8 +1,22 @@
 package se.kth.sda6.todo.tasks;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task")
 public class Task {
+    @Id
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "date")
     private String date;
 
     public Task() {
--- src/main/java/se/kth/sda6/todo/tasks/TaskService.java
+++ src/main/java/se/kth/sda6/todo/tasks/TaskService.java
@@ -1,8 +1,8 @@
 package se.kth.sda6.todo.tasks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -10,41 +10,28 @@ import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
-    private static Long idCounter = 1L;
-    private static List<Task> taskList = new ArrayList<>();
-    static {
-        add(new Task(null, "name 1", "2020.01.01"));
-        add(new Task(null, "name 2", "2020.01.02"));
-    }
+    @Autowired
+    private TaskRepository repository;
 
-    // Get a list of all the tasks
+    private static Long idCounter = 1L;
+
     public List<Task> getAll(String sort) {
-        return taskList.stream()
+        return repository.findAll().stream()
                 .sorted(Comparator.comparing(sort.equals("name") ? Task::getName : Task::getDate))
                 .collect(Collectors.toList());
     }
 
-    // Get a specific task by it's id
     public Optional<Task> getById(Long id) {
-        return taskList.stream()
-                .filter(t -> t.getId().equals(id))
-                .findFirst();
+        return repository.findById(id);
     }
 
     public Task create(Task newTask) {
-        add(newTask);
-        return newTask;
-    }
-
-    static private boolean add(Task newTask) {
         newTask.setId(idCounter);
         idCounter++;
-        return taskList.add(newTask);
+        return repository.save(newTask);
     }
 
     public void delete(Long id) {
-        taskList = taskList.stream()
-                .filter(t -> !t.getId().equals(id))
-                .collect(Collectors.toList());
+        repository.deleteById(id);
     }
 }
