600d0530736be73e469cc20a72427132b36ee17b
Ayham Alkazaz <ayham.alkazaz@voiapp.io>
2019-11-10 17:37:40 +0100

    5. creating and deleting tasks

--- src/main/java/se/kth/sda6/todo/tasks/TaskController.java
+++ src/main/java/se/kth/sda6/todo/tasks/TaskController.java
@@ -2,31 +2,38 @@ package se.kth.sda6.todo.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     @Autowired
     private TaskService taskService;
 
     // Get a list of all the tasks
-    @RequestMapping("/tasks")
-    public List<Task> getAll() {
-        return taskService.getAll();
+    @GetMapping("")
+    public List<Task> getAll(@RequestParam String sort) {
+        return taskService.getAll(sort);
     }
 
     // Get a specific task by it's id
-    @RequestMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     public Task getById(@PathVariable Long id) {
         return taskService.getById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     // Create a task
+    @PostMapping("")
+    public Task create(@RequestBody Task newTask) {
+        return taskService.create(newTask);
+    }
 
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        taskService.delete(id);
+    }
 }
--- src/main/java/se/kth/sda6/todo/tasks/TaskService.java
+++ src/main/java/se/kth/sda6/todo/tasks/TaskService.java
@@ -3,20 +3,25 @@ package se.kth.sda6.todo.tasks;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
+    private static Long idCounter = 1L;
     private static List<Task> taskList = new ArrayList<>();
     static {
-        taskList.add(new Task(1L, "name 1", "2020.01.01"));
-        taskList.add(new Task(2L, "name 2", "2020.01.02"));
+        add(new Task(null, "name 1", "2020.01.01"));
+        add(new Task(null, "name 2", "2020.01.02"));
     }
 
     // Get a list of all the tasks
-    public List<Task> getAll() {
-        return taskList;
+    public List<Task> getAll(String sort) {
+        return taskList.stream()
+                .sorted(Comparator.comparing(sort.equals("name") ? Task::getName : Task::getDate))
+                .collect(Collectors.toList());
     }
 
     // Get a specific task by it's id
@@ -24,5 +29,22 @@ public class TaskService {
         return taskList.stream()
                 .filter(t -> t.getId().equals(id))
                 .findFirst();
+    }
+
+    public Task create(Task newTask) {
+        add(newTask);
+        return newTask;
+    }
+
+    static private boolean add(Task newTask) {
+        newTask.setId(idCounter);
+        idCounter++;
+        return taskList.add(newTask);
+    }
+
+    public void delete(Long id) {
+        taskList = taskList.stream()
+                .filter(t -> !t.getId().equals(id))
+                .collect(Collectors.toList());
     }
 }
